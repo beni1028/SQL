fb9c7a538ada75b82ec19b676872b9f21c667db4
beni1028 <beni1028@gmail.com>
2023-03-05 21:24:27 +0530

    3

---
+++ #3.sql
@@ -0,0 +1,11 @@
+/*
+Challenge #3:
+
+Assuming there is a table named "employees" 
+with columns "employee_id", "first_name", "last_name", "department_id", and "hire_date", 
+write a SQL query to retrieve the "employee_id", "first_name", and "last_name" columns 
+for all employees who were hired before January 1st, 2022 and 
+belong to the department with ID 10. Order the results by the "hire_date" in ascending order.
+
+*/
+
